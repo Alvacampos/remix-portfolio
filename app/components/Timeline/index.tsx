17d5df3e1095d06f0f976f4e7aab73f868088421
Gonzalo Alvarez Campos <gonzaloralvarezcampos@gmail.com>
2025-05-06 15:51:09 -0300

    attempt to fix false rerender

--- app/components/Timeline/index.tsx
+++ app/components/Timeline/index.tsx
@@ -1,7 +1,6 @@
 import { getClassMaker } from '~/utils/utils';
 import pkg, { VerticalTimeline } from 'react-vertical-timeline-component';
 import { Link } from '@remix-run/react';
-import { v4 as uuid } from 'uuid';
 import { SuccessFilled } from '~/components/icons';
 import Card, { links as cardLinks } from '~/components/Card';
 import styles from './style.css?url';
@@ -34,13 +33,12 @@ export default function Timeline({ filteredData }: FilteredDataTypes) {
     <div className={getClasses()}>
       <VerticalTimeline>
         {filteredData.map((item) => {
-          const key = uuid();
           return (
             <VerticalTimelineElement
               className={getClasses('element')}
               date={item.date}
               icon={<SuccessFilled />}
-              key={key}
+              key={item.id}
             >
               <Link
                 to={`/skills/${item.id}`}
